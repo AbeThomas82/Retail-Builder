8715a5d3aea5756dbbbf07ebf21283fe97dae510
Sherwin Abraham <sherwinabraham@gmail.com>
2023-08-17 16:45:49 -0500

    Tinkering with category-routes more.

--- assets/routes/api/category-routes.js
+++ assets/routes/api/category-routes.js
@@ -19,9 +19,18 @@ router.get('/', async (req, res) => {
 
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  const foundID = await Category.findOne({
+    include: [Product]
+  })
+  if (!foundID){
+    return res.status(400).json({
+      message: "Cannot find ID."
+    })
+  }
+  return res.status(200).json(foundID)
 });
 
 router.post('/', (req, res) => {
