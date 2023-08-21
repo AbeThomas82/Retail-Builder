33a78c6bd38d5c37302c903c6f57ae4aac3d951d
Sherwin Abraham <sherwinabraham@gmail.com>
2023-08-21 11:40:52 -0500

    Removed superfluous assets folder.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
-const sequelize = require('../assets/config/connection');
+const sequelize = require('../Retail-Builder/config/connection');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
